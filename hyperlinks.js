f4508fff729ab71811b51311f3f137358d07791f
DrMagenta <piotrhus.2005@gmail.com>
2022-04-24 00:23:28 +0200

    Button to the current page is faded out (JS)

--- hyperlinks.js
+++ hyperlinks.js
@@ -1,23 +1,44 @@
 /*This add anchor hyperlinks to all buttons in the panel*/
 
-var buttons = document.getElementsByClassName('button')
-var subpages = [ // hrefs fro the anchors
+var buttons = document.getElementsByClassName('button');
+var subpages = [ // hrefs from the anchors
+  'index.html',
   'map.html',
   'usimages.html',
   'usimages.html',
   'usimages.html',
   'usimages.html'
-]
+];
 
+function getPureFilename(path) {
+  /*This function takes the path to a file as input (it looks sort of like
+  this: file:///D:/Piotrek/AniaDarekStrona/index.html) and returns pure
+  filename (index.html here)*/
+  let slash_inx = path.lastIndexOf('/'); // Pure filename begins after last slash in the path
+  path = path.slice(slash_inx + 1); //Don't include the slash
+  return path;
+};
+
+// This 'wraps' all the buttons with anchors to appropriate subpages
 for (var i = 0; i < buttons.length; i++) {
-  console.log('This is executed')
-  let button = buttons[i]
-  let a = document.createElement('a')
-  a.href = subpages[i]
+  let button = buttons[i];
+  let a = document.createElement('a');
+  a.href = subpages[i];
 
-  console.log(button)
+  button.parentNode.replaceChild(a, button);
+  a.appendChild(button);
 
-  button.parentNode.replaceChild(a, button)
-  a.appendChild(button)
+};
 
-}
+// This fades out button referring to this page, so you know which page you are on
+buttons = document.getElementsByClassName('button'); // Var 'buttons' gotta be refreshed for some reasons
+for (var i = 0; i < buttons.length; i++) {
+  let button = buttons[i];
+  let buttonHref = getPureFilename(button.parentNode.href); // Get the path to a button's subpage and simplify it
+  let me = getPureFilename(window.location.pathname); // Get the name of this page and simplify it
+
+  // Does the button refer to this page?
+  if (me === buttonHref) {
+    button.style = "opacity: 70%;";
+  };
+};
