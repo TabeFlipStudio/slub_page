0274dd8e38be5b2b95fca8fafc107891a9ca867a
DrMagenta <piotrhus.2005@gmail.com>
2022-05-17 21:03:44 +0200

    Reworked the booking system - it no longer uses <form>, in favour of JavaScript

---
+++ booking.js
@@ -0,0 +1,31 @@
+/*This is called after clicking the sumbit button; it gets the input values from the textboxes and returns them*/
+function getBookInput() {
+  let output = '';
+  var inputs = document.getElementsByTagName('input');
+  inputs = [].slice.call(inputs); // Arrayify the HTMLCollection (return of getElementsByTagName)
+  inputs.pop(); // Remove the Submit button from the array
+
+  for (var i = 0; i < inputs.length; i++) {
+    let data = inputs[i].value;
+    output += data + '; ';
+  };
+
+  return output;
+};
+
+/*This sends the book request to Integromat, which then sends it to Discord Mailbox*/
+function sendBookRequest() {
+  var request = new XMLHttpRequest();
+
+  request.open("POST", 'https://hook.eu1.make.com/is95jv15aip7mr9nuudvmksxbm5brk9o');
+  request.setRequestHeader('Content-type', 'application/json');
+
+  var parameters = {
+    username: 'Webhook-to-Discord',
+    avatar_url: '',
+    content: getBookInput()
+  };
+
+  let output = JSON.stringify(parameters);
+  request.send(output);
+};
